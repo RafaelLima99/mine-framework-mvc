f8faeda5d23502ffc6cef883ba4e0177eeed98fe
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-23 15:10:49 -0200

    db-mvc.sql

--- db-mvc.sql
+++ db-mvc.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 14-Dez-2020 às 12:51
--- Versão do servidor: 10.4.14-MariaDB
--- versão do PHP: 7.4.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `mvc`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_info`
---
 
 CREATE TABLE `tb_info` (
   `id` int(11) NOT NULL,
@@ -33,18 +8,9 @@ CREATE TABLE `tb_info` (
   `descricao` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `tb_info`
---
 
 INSERT INTO `tb_info` (`id`, `titulo`, `descricao`) VALUES
 (1, 'aaaa', 'bbbbb');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_produtos`
---
 
 CREATE TABLE `tb_produtos` (
   `id` int(11) NOT NULL,
@@ -52,46 +18,22 @@ CREATE TABLE `tb_produtos` (
   `preco` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `tb_produtos`
---
 
 INSERT INTO `tb_produtos` (`id`, `descricao`, `preco`) VALUES
 (1, 'produto1', 55);
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `tb_info`
---
 ALTER TABLE `tb_info`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `tb_produtos`
---
+
 ALTER TABLE `tb_produtos`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `tb_info`
---
 ALTER TABLE `tb_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de tabela `tb_produtos`
---
 ALTER TABLE `tb_produtos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
